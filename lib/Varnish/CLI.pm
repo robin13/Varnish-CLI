a3423409084bf596e467e939023decd3d0d962d9
Robin Clarke <robin@robinclarke.net>
2011-03-13 12:23:40 +0100

    Improved documentation

--- lib/Varnish/CLI.pm
+++ lib/Varnish/CLI.pm
@@ -10,11 +10,11 @@ Varnish::CLI - An interface to the Varnish CLI
 
 =head1 VERSION
 
-Version::CLI 0.01
+Version::CLI 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
